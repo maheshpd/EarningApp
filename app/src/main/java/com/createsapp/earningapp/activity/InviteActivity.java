bbe3080e93309a06b758115197aa8203b6cb2295
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-08-27 13:19:16 +0530

    Refer & Earn || Part 13

--- app/src/main/java/com/createsapp/earningapp/activity/InviteActivity.java
+++ app/src/main/java/com/createsapp/earningapp/activity/InviteActivity.java
@@ -17,6 +17,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.createsapp.earningapp.R;
+import com.createsapp.earningapp.model.ProfileModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -25,6 +28,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class InviteActivity extends AppCompatActivity {
 
@@ -44,20 +49,50 @@ public class InviteActivity extends AppCompatActivity {
 
         init();
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         reference = FirebaseDatabase.getInstance().getReference().child("Users");
 
         loadData();
+        redeemAvailablility();
         clickListener();
+    }
+
+    private void redeemAvailablility() {
+        reference.child(user.getUid())
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                        if (snapshot.exists() && snapshot.hasChild("redeemed")) {
+
+                            boolean isAvailable = snapshot.child("redeemed").getValue(Boolean.class);
+
+                            if (isAvailable) {
+                                redeemBtn.setVisibility(View.GONE);
+                                redeemBtn.setEnabled(false);
+                            } else {
+                                redeemBtn.setEnabled(true);
+                                redeemBtn.setVisibility(View.VISIBLE);
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
     }
 
     private void init() {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         referCodeTv = findViewById(R.id.referCodeTv);
         shareBtn = findViewById(R.id.shareBtn);
+        redeemBtn = findViewById(R.id.redeemBtn);
 
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
@@ -134,7 +169,7 @@ public class InviteActivity extends AppCompatActivity {
                         if (inputCode.equals(referCodeTv.getText().toString())) {
                             Toast.makeText(InviteActivity.this, "You can not input your own code", Toast.LENGTH_SHORT).show();
                         }
-                        redeemQuery(inputCode);
+                        redeemQuery(inputCode, dialogInterface);
                     }
                 }).setNegativeButton("No", new DialogInterface.OnClickListener() {
                     @Override
@@ -151,8 +186,62 @@ public class InviteActivity extends AppCompatActivity {
 
     }
 
-    private void redeemQuery(String inputCode) {
-        Query query = reference.orderByChild("referCode");
+    private void redeemQuery(String inputCode, final DialogInterface dialogInterface) {
+        Query query = reference.orderByChild("referCode").equalTo(inputCode);
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    opposite = dataSnapshot.getKey();
+
+                    reference
+                            .addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    ProfileModel model = snapshot.child(opposite).getValue(ProfileModel.class);
+                                    ProfileModel myModel = snapshot.child(opposite).getValue(ProfileModel.class);
+
+                                    int coins = model.getCoins();
+                                    int updateCoins = coins + 100;
+
+                                    int myCoins = myModel.getCoins();
+                                    int myUpdate = myCoins + 100;
+
+                                    HashMap<String, Object> map = new HashMap<>();
+                                    map.put("coins", updateCoins);
+
+                                    HashMap<String, Object> myMap = new HashMap<>();
+                                    myMap.put("coins", myUpdate);
+                                    myMap.put("redeemed", true);
+
+                                    reference.child(opposite).updateChildren(map);
+                                    reference.child(user.getUid()).updateChildren(myMap)
+                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                    dialogInterface.dismiss();
+                                                    Toast.makeText(InviteActivity.this, "Congrates", Toast.LENGTH_SHORT).show();
+                                                }
+                                            });
+
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                }
+                            });
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(InviteActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
--- app/src/main/java/com/createsapp/earningapp/activity/MainActivity.java
+++ app/src/main/java/com/createsapp/earningapp/activity/MainActivity.java
@@ -60,6 +60,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, ProfileActivity.class));
             }
         });
+
+        referCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, InviteActivity.class));
+            }
+        });
     }
 
     private void init() {
