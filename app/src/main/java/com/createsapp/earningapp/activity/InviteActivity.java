4274dfed2faec170364200e613877b5e15899ee8
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-08-27 12:54:58 +0530

    Part 12

--- app/src/main/java/com/createsapp/earningapp/activity/InviteActivity.java
+++ app/src/main/java/com/createsapp/earningapp/activity/InviteActivity.java
@@ -1,15 +1,30 @@
 package com.createsapp.earningapp.activity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.createsapp.earningapp.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class InviteActivity extends AppCompatActivity {
 
@@ -19,7 +34,8 @@ public class InviteActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private TextView referCodeTv;
-    private Button shareBtn;
+    DatabaseReference reference;
+    private Button shareBtn, redeemBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +46,10 @@ public class InviteActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        loadData();
+        reference = FirebaseDatabase.getInstance().getReference().child("Users");
 
+        loadData();
+        clickListener();
     }
 
     private void init() {
@@ -50,4 +68,92 @@ public class InviteActivity extends AppCompatActivity {
         onBackPressed();
         return true;
     }
+
+    private void loadData() {
+        reference.child(user.getUid())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        String referCode = snapshot.child("referCode").getValue(String.class);
+                        referCodeTv.setText(referCode);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Toast.makeText(InviteActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                });
+
+    }
+
+    private void clickListener() {
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String referCode = referCodeTv.getText().toString();
+
+                String shareBody = "Hey, i'm using the best earning app. Join using my invite code to instantly get 100"
+                        + "coins. My invite code is  " + referCode + "\n" +
+                        "Download from Play Store\n" +
+                        "https://play.google.com/store/apps/details?id=" +
+                        getPackageName();
+
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, shareBody);
+                startActivity(intent);
+            }
+        });
+
+        redeemBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final EditText editText = new EditText(InviteActivity.this);
+                editText.setHint("abc12");
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT);
+
+                editText.setLayoutParams(layoutParams);
+
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(InviteActivity.this);
+                alertDialog.setTitle("Redeem Code");
+
+                alertDialog.setView(editText);
+
+                alertDialog.setPositiveButton("Redeem", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String inputCode = editText.getText().toString();
+                        if (TextUtils.isEmpty(inputCode)) {
+                            Toast.makeText(InviteActivity.this, "Input valid code", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        if (inputCode.equals(referCodeTv.getText().toString())) {
+                            Toast.makeText(InviteActivity.this, "You can not input your own code", Toast.LENGTH_SHORT).show();
+                        }
+                        redeemQuery(inputCode);
+                    }
+                }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        dialogInterface.dismiss();
+
+                    }
+                });
+
+                alertDialog.show();
+            }
+        });
+
+    }
+
+    private void redeemQuery(String inputCode) {
+        Query query = reference.orderByChild("referCode");
+    }
+
+
 }
